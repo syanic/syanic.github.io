e9348036c9acdd93ac523f0ec30146d44d6b101e
Syanic XD <94116695+fspofficial@users.noreply.github.com>
2024-09-20 19:41:32 +0530

    Update ptb_setup.sh

--- files/ptb_setup.sh
+++ files/ptb_setup.sh
@@ -6,14 +6,14 @@
 
 echo "===== SYANIC UTILITY ====="
 echo "Made by Syanic XD on YouTube <3"
-echo
+echo ""
 echo "Options:"
 echo "1. Setup Openbox + Chrome Remote Connection + Discord PTB"
-echo "2. Exit"
+echo "2. Start Chrome Remote Connection (ONLY USE IF ALREADY SETUP)"
+echo "3. Exit"
+read -p "Select an option [1-3]: " option
 
-read -p "Select an option (1 or 2): " option
-
-if [ "$option" -eq 1 ]; then
+if [[ $option -eq 1 ]]; then
     echo "### Updating System.. [1/2]"
     sudo apt-get update
     echo "### Updated System.. [1/2]"
@@ -21,8 +21,8 @@ if [ "$option" -eq 1 ]; then
     echo "### Upgrading System.. [2/2]"
     sudo apt-get upgrade -y
     echo "### Upgraded System.. [2/2]"
-
-    echo "### Installing Discord PTB, Chromium & Chrome Remote Desktop.."
+    
+    echo "### Installing Discord PTB, Chromium & Chrome Remote Desktop..."
     wget -O discord-ptb.deb "https://discordapp.com/api/download/ptb?platform=linux&format=deb"
     wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
     
@@ -30,28 +30,36 @@ if [ "$option" -eq 1 ]; then
     sudo apt-get install chromium-browser -y
     sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
 
-    echo "### Installing Openbox with English Keyboard.."
+    echo "### Installing Openbox with English Keyboard..."
     sudo apt-get install openbox -y
-    sudo apt-get install xkb-utils -y
-    echo "### Installed Openbox with English Keyboard."
+    echo "### Openbox Installed."
 
     read -p "Enter your Chrome Remote Desktop SSH Command > " ssh_command
-    
+
     while true; do
         echo "You entered: $ssh_command"
         read -p "Is this correct? (y/n): " confirm
-        if [[ "$confirm" == "y" ]]; then
+        if [[ $confirm == "y" || $confirm == "Y" ]]; then
             break
         else
             read -p "Enter your Chrome Remote Desktop SSH Command > " ssh_command
         fi
     done
 
-    modified_command=$(echo "$ssh_command" | sed 's/--name=.*$/--name="Sub to Syanic XD"/')
-    
-    echo "### Running Chrome Remote Desktop Command.."
-    eval "$modified_command"
-else
+    modified_command=${ssh_command/--name=*/--name="Sub to Syanic XD"}
+    echo "### Starting Chrome Remote Connection..."
+    $modified_command
+
+elif [[ $option -eq 2 ]]; then
+    echo "Please ensure that Chrome Remote Desktop is already set up."
+    read -p "Enter your Chrome Remote Desktop SSH Command > " ssh_command
+    $ssh_command
+
+elif [[ $option -eq 3 ]]; then
     echo "Exiting..."
     exit 0
+
+else
+    echo "Invalid option selected."
+    exit 1
 fi
