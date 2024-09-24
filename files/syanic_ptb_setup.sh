2d72e96d9e4385e793c1df48d84218ac5cb319ec
Syanic XD <94116695+fspofficial@users.noreply.github.com>
2024-09-24 13:22:34 +0530

    Update syanic_ptb_setup.sh

--- files/syanic_ptb_setup.sh
+++ files/syanic_ptb_setup.sh
@@ -24,8 +24,8 @@ setup() {
     sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
     
     echo "### SU: Installing Openbox with English Keyboard.."
-    echo "keyboard-configuration  keyboard-configuration/layoutcode  select  us" | sudo debconf-set-selections
-    sudo sed -i 's/XKBLAYOUT=.*/XKBLAYOUT="us"/' /etc/default/keyboard
+    echo "keyboard-configuration keyboard-configuration/layoutcode string us" | sudo debconf-set-selections
+    echo "keyboard-configuration keyboard-configuration/xkb-keymap select us" | sudo debconf-set-selections
     sudo apt-get install openbox -y
     echo "### SU: Installed Openbox with English Keyboard."
     
